7cea77a62f69acecd41fadc99f3803abe5663e1e
Josh Ferge <josh.ferge@gmail.com>
2023-05-24 18:57:25 -0700

    init

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@ const { exec } = require("child_process");
 
 async function installClickhouse(path: string) {
   return new Promise((resolve, reject) => {
-    console.log(path);
     exec(
       `[ -f "${path}/clickhouse" ] && exit 0 || cd ${path} && curl https://clickhouse.com/ | sh`,
       // @ts-ignore
@@ -25,47 +24,40 @@ async function installClickhouse(path: string) {
   });
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+async function formatQuery(editor: vscode.TextEditor, extensionPath: string) {
+  const document = editor.document;
+  const text = document.getText();
+  const command = `${extensionPath}/clickhouse format --query "${text}"`;
+
+  // @ts-ignore
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      vscode.window.showErrorMessage(`Error formatting query: ${stderr}`);
+      return;
+    }
+
+    const fullRange = new vscode.Range(
+      document.positionAt(0),
+      document.positionAt(text.length)
+    );
+
+    editor.edit((editBuilder) => {
+      editBuilder.replace(fullRange, stdout);
+      vscode.window.showInformationMessage("Formatted Clickhouse Query");
+    });
+  });
+}
+
 export async function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   await installClickhouse(context.extensionPath);
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+
+  const disposable = vscode.commands.registerCommand(
     "clickhouse-query-formatter.formatClickhouseQuery",
     async () => {
-      const editor = vscode.window.activeTextEditor; // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
+
       if (editor) {
-        const document = editor.document;
-        const text = document.getText();
-
-        exec(
-          `${context.extensionPath}/clickhouse format --query "${text}"`,
-          // @ts-ignore
-          (error, stdout, stderr) => {
-            if (error) {
-              vscode.window.showErrorMessage(
-                `Error formatting query: ${stderr}`
-              );
-              return;
-            }
-
-            editor.edit((editBuilder) => {
-              const fullRange = new vscode.Range(
-                document.positionAt(0),
-                document.positionAt(text.length)
-              );
-
-              editBuilder.replace(fullRange, stdout);
-              vscode.window.showInformationMessage(
-                "Formatted Clickhouse Query"
-              );
-            });
-          }
-        );
+        await formatQuery(editor, context.extensionPath);
       }
     }
   );
